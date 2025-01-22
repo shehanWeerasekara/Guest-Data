ac661112c56e613c3858a62e395b66a21bd09994
shehan weerasekara <tharindushehan732@gmail.com>
2025-01-22 10:23:14 +0530

    write,read module test

--- Test/core_test.js
+++ Test/core_test.js
@@ -1,4 +1,7 @@
 const fs = require('node:fs');
 
-fs.writeFileSync("data.txt","hello");
-fs.appendFileSync("data.txt"," world");
+// fs.writeFileSync("data.txt","hello");
+// fs.appendFileSync("data.txt"," world");
+
+const data = fs.readFileSync("data.txt");
+console.log(data.toString());
