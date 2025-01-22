b30b0a73d8126c359dbf3367cdd74da1b5482b45
shehan weerasekara <tharindushehan732@gmail.com>
2025-01-22 10:27:16 +0530

    os module test

--- Test/core_test.js
+++ Test/core_test.js
@@ -1,7 +1,13 @@
 const fs = require('node:fs');
+const os = require('node:os');
 
 // fs.writeFileSync("data.txt","hello");
 // fs.appendFileSync("data.txt"," world");
 
-const data = fs.readFileSync("data.txt");
-console.log(data.toString());
+// const data = fs.readFileSync("data.txt");
+// console.log(data.toString());
+
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.uptime());
+
