50b83a8fb001320f5f37d0cad18150947e3dfa96
shehan weerasekara <tharindushehan732@gmail.com>
2025-01-23 11:57:54 +0530

    test

--- Test/core_test.js
+++ Test/core_test.js
@@ -1,13 +1,13 @@
 const fs = require('node:fs');
 const os = require('node:os');
 
-// fs.writeFileSync("data.txt","hello");
-// fs.appendFileSync("data.txt"," world");
+//save
+const guests = [];
+guests.push({
+    name: "shehan",
+    address:"kandy"
+});
+//console.log(guests);
 
-// const data = fs.readFileSync("data.txt");
-// console.log(data.toString());
-
-// console.log(os.freemem());
-// console.log(os.totalmem());
-// console.log(os.uptime());
-
+const dataJSON = JSON.stringify(guests);
+fs.writeFileSync("j_data.json",dataJSON);
